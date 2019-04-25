4be2d6a82194f817f371134200f9d97860dce902
fangbao <fangbao@i-counting.cn>
2019-04-25 18:19:07 +0800

    [fix] 优化绑定页面 related to issue #1

--- dev/iframe.js
+++ dev/iframe.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import Config from '../components/config'
 import Iframe from '../components/iframe/index'
-import {
-  BrowserRouter,
+import
+{
+  HashRouter,
   Route,
   Switch
 } from 'react-router-dom'
-const Router = BrowserRouter
-Config.env = 'production'
+const Router = HashRouter
+Config.env = 'development'
 class Main extends React.Component {
   render () {
     return (
-      <div style={{height: '100%'}}>
+      <div style={ { height: '100%' } }>
         <Router>
-          <Switch>
-            <Iframe
-              token='103a43c0-b801-4b3e-adf9-5814d430ab78'
-              // env='production'
-            >
-              <Route path='/organ' render={() => <div>organ</div> }/>
-            </Iframe>
-          </Switch>
+          <Iframe
+            token='24d64163-1804-44db-9723-70ae419a5eef'
+            env='development'
+          >
+          </Iframe>
         </Router>
       </div>
     )
